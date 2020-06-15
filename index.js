30bdd7bc1afcd4564ff6714e7fe9d7910efe7b09
Carlos Palmero <palmeroh@gmail.com>
2020-06-15 14:08:27 -0500

    fix default id when not yet identified (#8)

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ export default class ExpoMixpanelAnalytics {
   }
 
   getDistinctId() {
-    return this.userId;
+    return this.userId || this.properties.distinct_id;
   }
 
   // ===========================================================================================

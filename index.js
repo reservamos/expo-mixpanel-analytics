e90ce33a82d8f8209d4f68681b2aff13725ef1d0
ErnestoVM <arkangel_121@hotmail.com>
2022-02-10 11:28:32 -0600

    add `react-native-async-storage` community package

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-import { Platform, Dimensions, AsyncStorage } from 'react-native';
-import Constants from 'expo-constants';
-import * as Updates from 'expo-updates';
-import { Buffer } from 'buffer';
-import UUID from 'uuid/v1';
-import pkg from './package.json';
-import getIP from './src/getIP';
+import { Platform, Dimensions } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Constants from "expo-constants";
+import * as Updates from "expo-updates";
+import { Buffer } from "buffer";
+import UUID from "uuid/v1";
+import pkg from "./package.json";
+import getIP from "./src/getIP";
 
-const UUID_STORAGE = '@MP_RESERVAMOS_LIB_STORAGE:UUID';
-const MIXPANEL_API_URL = 'http://api.mixpanel.com';
+const UUID_STORAGE = "@MP_RESERVAMOS_LIB_STORAGE:UUID";
+const MIXPANEL_API_URL = "http://api.mixpanel.com";
 const MIXPANEL_API_URL_TRACK = `${MIXPANEL_API_URL}/track/?data=`;
 const MIXPANEL_API_URL_ENGAGE = `${MIXPANEL_API_URL}/engage/?data=`;
 
@@ -40,17 +41,17 @@ export default class ExpoMixpanelAnalytics {
   }
 
   _parseUserAgent(userAgent) {
-    if (!userAgent || userAgent.split(';').length !== 4) return {};
+    if (!userAgent || userAgent.split(";").length !== 4) return {};
     return {
-      osVersion: userAgent.split(';')[2].trim(),
-      model: userAgent.split(';')[3].trim().slice(0, -1),
+      osVersion: userAgent.split(";")[2].trim(),
+      model: userAgent.split(";")[3].trim().slice(0, -1),
     };
   }
 
   async _setProperties() {
     this.properties = {
       token: this.token,
-      mp_lib: 'React Native Reservamos',
+      mp_lib: "React Native Reservamos",
       $lib_version: pkg.version,
     };
 
@@ -58,28 +59,28 @@ export default class ExpoMixpanelAnalytics {
       const ipResponse = await getIP();
       this.properties.ip = ipResponse.ip;
 
-      const { width, height } = Dimensions.get('window');
+      const { width, height } = Dimensions.get("window");
       this.properties.$screen_width = `${width}`;
       this.properties.$screen_height = `${height}`;
       this.properties.$device_id = Constants.installationId;
       this.properties.$app_version_string = Updates?.manifest?.version;
       this.properties.distinct_id = await this._getUUID();
-      this.properties['$user_id'] = await this._getUUID();
+      this.properties["$user_id"] = await this._getUUID();
       this.properties.$browser = await Constants.getWebViewUserAgentAsync();
     } catch (e) {
       console.log(e);
     }
 
-    if (Platform.OS === 'ios') {
-      this.properties.$os = 'iOS';
+    if (Platform.OS === "ios") {
+      this.properties.$os = "iOS";
       this.properties.platform = Constants.platform.ios.platform;
       this.properties.$os_version = Constants.platform.ios.systemVersion;
       this.properties.$model = Constants.platform.ios.model;
       this.properties.$ios_app_version = Updates?.manifest?.version;
     } else {
-      this.properties.$os = 'Android';
-      this.properties.platform = 'android';
-      this.properties['Android API Version'] = Platform.Version;
+      this.properties.$os = "Android";
+      this.properties.platform = "android";
+      this.properties["Android API Version"] = Platform.Version;
       const { osVersion, model } = this._parseUserAgent(
         this.properties.$browser
       );
@@ -102,12 +103,12 @@ export default class ExpoMixpanelAnalytics {
 
   identify(userId, traits) {
     const id = userId || this.properties.distinct_id;
-    this.track('$identify', {
+    this.track("$identify", {
       distinct_id: id,
       $anon_distinct_id: this.properties.distinct_id,
     });
     this.userId = id;
-    this.properties['$user_id'] = id;
+    this.properties["$user_id"] = id;
     this.properties.distinct_id = id;
     if (traits) {
       this.people_set(traits);
@@ -116,7 +117,7 @@ export default class ExpoMixpanelAnalytics {
   }
 
   alias(alias, traits) {
-    this.track('$create_alias', {
+    this.track("$create_alias", {
       alias: alias,
       distinct_id: this.properties.distinct_id,
     });
@@ -128,37 +129,37 @@ export default class ExpoMixpanelAnalytics {
     const uuid = UUID();
     this.properties.distinct_id = uuid;
     this.properties.$device_id = uuid;
-    this.properties['$user_id'] = uuid;
+    this.properties["$user_id"] = uuid;
     this.userId = null;
     this._saveUUID(uuid);
   }
 
   people_set(props) {
-    this._people('set', props);
+    this._people("set", props);
   }
 
   people_set_once(props) {
-    this._people('set_once', props);
+    this._people("set_once", props);
   }
 
   people_unset(props) {
-    this._people('unset', props);
+    this._people("unset", props);
   }
 
   people_increment(props) {
-    this._people('add', props);
+    this._people("add", props);
   }
 
   people_append(props) {
-    this._people('append', props);
+    this._people("append", props);
   }
 
   people_union(props) {
-    this._people('union', props);
+    this._people("union", props);
   }
 
   people_delete_user() {
-    this._people('delete', '');
+    this._people("delete", "");
   }
 
   getDistinctId() {
@@ -194,13 +195,13 @@ export default class ExpoMixpanelAnalytics {
       properties: { ...this.properties, ...event.props },
     };
 
-    data = new Buffer(JSON.stringify(data)).toString('base64');
+    data = new Buffer(JSON.stringify(data)).toString("base64");
 
     return fetch(MIXPANEL_API_URL_TRACK + data);
   }
 
   _pushProfile(data) {
-    data = new Buffer(JSON.stringify(data)).toString('base64');
+    data = new Buffer(JSON.stringify(data)).toString("base64");
     return fetch(MIXPANEL_API_URL_ENGAGE + data);
   }
 }
